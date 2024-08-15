c5a47aaa32134f40318a6f774f612984883daeff
Lavanya1310k <lakshmilavanyakaribandi23@gmail.com>
2024-08-15 14:42:38 +0530

    push

---
+++ test/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ProductsTable from "./ProductsTable.js";
+
+const App = () => (
+  <div className="App">
+      <ProductsTable/>
+    </div>
+);
+export default App;
